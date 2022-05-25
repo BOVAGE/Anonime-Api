2c65d45bd89e888a727705497321f0298ca6e544
bovage <fayemibolu2@gmail.com>
2022-05-25 04:20:30 -1200

    feat: add 404 error view  & exceptions handler

---
+++ common/errors.py
@@ -0,0 +1,10 @@
+from rest_framework import status
+from django.http import JsonResponse
+
+def not_found(request, exception, *args, **kwargs):
+    """ Generic 404 error handler """
+    data = {
+        'error': 'Not Found (404)'
+    }
+    return JsonResponse(data, status=status.HTTP_404_NOT_FOUND)
+    
